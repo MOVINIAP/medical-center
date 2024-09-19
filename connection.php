625735f6e7ded03d90d812232a7996c2a3130d3a
Rino Arias <rinoariaslaaz@gmail.com>
2024-09-18 21:50:43 -0500

    modificando base de datos

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
     // $database= new mysqli("localhost","root","12345678","edoc");
-    $database= new mysqli("localhost","root","","edoc");
+    $database= new mysqli("localhost","root","12345678","edoc");
     if ($database->connect_error){
         die("Conexión fallida:  ".$database->connect_error);
     }
